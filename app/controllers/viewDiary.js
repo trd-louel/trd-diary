46d89bbea03f8eea670fda8932d75ebbc60a31d2
trd-isaac <trd.isaac@gmail.com>
2014-09-24 11:33:10 +0800

    display the dates through tableview

--- app/controllers/viewDiary.js
+++ app/controllers/viewDiary.js
@@ -3,13 +3,15 @@
 
 //var table2 = authorCollection.config.adapter.collection_name('diary_notes');
 
-
-
 function init(){
+	getAuthorList();
+}
+
+function getAuthorList(){
 	var authorCollection = Alloy.createCollection('db_diary');
 	var table1 = authorCollection.config.adapter.collection_name;
 	authorCollection = Alloy.Collections.instance('db_diary');
-	
+	var tblView1 = Ti.UI.createTableView();
 	var sql = 'SELECT * FROM ' +table1;	
 	authorCollection.fetch({ query: sql});
 	
@@ -27,18 +29,20 @@ function init(){
 		row.addEventListener('click', getContent);
 		authorArr.push(row);	
 	}
-	$.authorTbl.setData(authorArr);	
+	tblView1.setData(authorArr);
+	$.viewDiary.add(tblView1);	
 	return false;
 	
 }
 
 function getContent(row_evt){
+	
 	var contentCollection = Alloy.createCollection('viewDiary');
 	var table = contentCollection.config.adapter.collection_name;
 	contentCollection = Alloy.Collections.instance('viewDiary');
-	var view = Ti.UI.createView();
-	view.accessibilityHidden=true;
-	var listView = Ti.UI.createListView();
+	//var view = Ti.UI.createView();
+	//view.accessibilityHidden=true;
+	var tblView = Ti.UI.createTableView();
 	
 	var sql = 'SELECT * FROM ' +table+ ' WHERE content_id=1';
 	contentCollection.fetch({ query: sql});
@@ -49,17 +53,18 @@ function getContent(row_evt){
 		
 		var id = content.get('content_id');
 		var date = content.get('date');
-		var list = Ti.UI.createListSection({
+		var list = Ti.UI.createTableViewRow({
 			'title' : date,
 			'c_id' : id  
 		});
 		contentArr.push(list);
 	}
-	var section = Ti.UI.createListSection();
-	section.setItems(contentArr);
-	listView.sections = [section];
-	view.add(listView);
-	view.accessibilityHidden=False;
+	//var section = Ti.UI.createTableViewRow();
+	
+	tblView.setData(contentArr);
+	tblView.appendRow(list);
+	$.viewDiary.add(tblView);
+	//tblView.show();
 	
 }
 
