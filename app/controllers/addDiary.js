d9ae5673bd01d99246b5a62d6f49305a0ce3fa95
trd-louel <trd.louel@gmail.com>
2014-09-26 14:10:16 +0800

    icon change; add diary

---
+++ app/controllers/addDiary.js
@@ -0,0 +1,8 @@
+	var authorCollection = Alloy.createCollection('db_diary');
+	var table1 = authorCollection.config.adapter.collection_name;
+	
+	var contentCollection = Alloy.createCollection('viewDiary');
+	var table = contentCollection.config.adapter.collection_name;
+	
+	
+	
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,11 @@
 $.navigation.open();
+
 function btnView(e){
-	 var getAuthors = Alloy.createController('viewDiary').getView();
+	var getAuthors = Alloy.createController('viewDiary').getView();
 	$.navigation.openWindow(getAuthors);
+}
+
+function btnAdd(e){
+	var addDiary = Alloy.createController('addDiary').getView();
+	$.navigation.openWindow(addDiary);
 }
