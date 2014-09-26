52f964fb3539ca1438d56628fe37325eb09cc001
trd-louel <trd.louel@gmail.com>
2014-09-26 16:37:21 +0800

    new app icon; add window

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,13 @@
 $.navigation.open();
-function btnView(e){
+
+function btnView(e)
+{
 	 var getAuthors = Alloy.createController('viewDiary').getView();
 	$.navigation.openWindow(getAuthors);
+}
+
+function btnAdd(e)
+{
+	 var getDiary = Alloy.createController('addDiary').getView();
+	$.navigation.openWindow(getDiary);
 }
