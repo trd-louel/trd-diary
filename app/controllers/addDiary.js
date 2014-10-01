018fd207fa75c7894db18ef12d28c188f38248dc
trd-louel <trd.louel@gmail.com>
2014-10-01 12:11:05 +0800

    get date; change nav bar color;

--- app/controllers/addDiary.js
+++ app/controllers/addDiary.js
@@ -4,30 +4,87 @@ var table1 = authorCollection.config.adapter.collection_name;
 var contentCollection = Alloy.createCollection('viewDiary');
 var table = contentCollection.config.adapter.collection_name;
 
-function getDate(i)
-{
-	if(i>=0&&i<=11)
-	{
-		var monthStrArr = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-		return monthStrArr[i];
+function getDate() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var month = currentTime.getMonth() + 1;
+    var day = currentTime.getDate();
+    var year = currentTime.getFullYear();
+ 
+ 	switch(month)
+ 	{
+ 		case 1:
+ 		case 01:
+ 			month = "January";
+ 			break;
+ 		case 2:
+ 		case 02:
+ 			month = "February";
+ 			break;
+ 		case 3:
+ 		case 03:
+ 			month = "March";
+ 			break;
+ 		case 4:
+ 		case 04:
+ 			month = "April";
+ 			break;
+ 		case 5:
+ 		case 05:
+ 			month = "May";
+ 			break;
+ 		case 6:
+ 		case 06:
+ 			month = "June";
+ 			break;
+ 		case 7:
+ 		case 07:
+ 			month = "July";
+ 			break;
+ 		case 8:
+ 		case 08:
+ 			month = "August";
+ 			break;
+ 		case 9:
+ 		case 09:
+ 			month = "September";
+ 			break;
+ 		case 10:
+ 			month = "October";
+ 			break;
+ 		case 11:
+ 			month = "November";
+ 			break;
+ 		case 12:
+ 			month = "December";
+ 	}
+ 	
+	if (hours < 10) 
+	{ 
+		hours = "0" + hours;
 	}
-	else
-	{
-		return '-';
+	 
+	if (minutes < 10) 
+	{ 
+		minutes = "0" + minutes;
 	}
+
+	return month + " " + day + ", " + year + " -  " + hours + ":" + minutes;
 }
 
-function back(e) 
+$.addTitle.value = getDate();
+
+function viewDiary(e) 
 {
-	$.addDiary.close();
+	//$.addDiary.close();
 }
 
 function saveDiary()
 {
-	var header = $.addTitle.value;
 	var note = $.addNotes.value;
 	
-	if(note.trim().length > 0 && header.trim().length > 0)
+	if(note.trim().length > 0)
 	{
 		var newDate = new Date();
 		
